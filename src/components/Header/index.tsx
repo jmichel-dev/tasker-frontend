9ffb4d5ef854895be1502bbe0ac5e6b75d2a4030
Jean Michel <jeaanmichel@protonmail.com>
2022-06-02 07:48:29 -0300

    defined handle for open and close the modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+import { Modal } from "../Modal";
 import { Container, HeaderContainer } from "./styles";
 
 export const Header: React.FC = () => {
+  const [openNewTaskModal, setOpenNewTaskModal] = useState(false);
+
+  const handleOpenNewTask = () => {
+    console.log("click");
+    setOpenNewTaskModal(true);
+  };
+
+  const handleCloseNewTask = () => {
+    setOpenNewTaskModal(false);
+  };
+
   return (
     <HeaderContainer>
       <Container>
         <h2>Tasker</h2>
-        <button>Nova task</button>
+        <button onClick={handleOpenNewTask}>Nova task</button>
       </Container>
+
+      <Modal isOpen={openNewTaskModal} onCloseModal={handleCloseNewTask}>
+        Hello, Im a modal
+      </Modal>
     </HeaderContainer>
   );
 };
