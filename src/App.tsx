40ca6db883796d0b9768f69b8fff136421f7049c
Jean Michel <jeaanmichel@protonmail.com>
2022-06-02 07:15:25 -0300

    added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Home } from "./pages/Home";
+import { GlobalStyle } from "./styles/global";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Home />
+      <GlobalStyle />
+    </>
   );
 }
 
