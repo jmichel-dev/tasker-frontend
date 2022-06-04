8bc07d3d3561f19c1bcf27fb681fed5d85218272
Jean Michel <jeaanmichel@protonmail.com>
2022-06-04 11:29:09 -0300

    defined task provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Home } from "./pages/Home";
 import ReactModal from "react-modal";
 import { GlobalStyle } from "./styles/global";
+import { TaskProvider } from "./context/TaskContext";
 
 ReactModal.setAppElement("#root");
 
 function App() {
   return (
-    <>
+    <TaskProvider>
       <Home />
       <GlobalStyle />
-    </>
+    </TaskProvider>
   );
 }
 
