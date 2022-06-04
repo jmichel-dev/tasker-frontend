28501885d0c9779198b80b6e2421488ae362fc88
Jean Michel <jeaanmichel@protonmail.com>
2022-06-04 15:11:55 -0300

    added function for formate date for difference in days

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,3 +1,5 @@
+import { differenceInDays } from "date-fns";
+
 export const formatCurrency = (value: number): string => {
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -7,4 +9,18 @@ export const formatCurrency = (value: number): string => {
 
 export const formatDate = (date: Date): string => {
   return new Intl.DateTimeFormat("pt-BR").format(new Date(date));
+};
+
+export const formatDateInDays = (date: Date): string => {
+  const today = new Date();
+
+  const valueDifferenceInDays = differenceInDays(today, date);
+
+  if (valueDifferenceInDays === 0) {
+    return "Hoje";
+  } else if (valueDifferenceInDays === 1) {
+    return "1 dia atrás";
+  } else {
+    return `${valueDifferenceInDays} dias atrás`;
+  }
 };
